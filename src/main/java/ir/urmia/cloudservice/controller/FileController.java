68c24e97cbb2f71e651c44175008b15cb6501ce7
mahdi-win10 <riza.rs2000@gmail.com>
2022-01-03 22:45:09 +0330

    add file rest controller

---
+++ src/main/java/ir/urmia/cloudservice/controller/FileController.java
@@ -0,0 +1,64 @@
+package ir.urmia.cloudservice.controller;
+
+import ir.urmia.cloudservice.domain.DBFile;
+import ir.urmia.cloudservice.payload.UploadFileResponse;
+import ir.urmia.cloudservice.service.DBFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private DBFileService fileService;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+
+        String username = "";
+        DBFile dbFile = fileService.storeFile(file, username);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(String.valueOf(dbFile.getId()))
+                .toUriString();
+
+        return new UploadFileResponse(dbFile.getFileName(), fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.stream(files)
+                .map(this::uploadFile)
+                .collect(Collectors.toList());
+    }
+
+    //    @RequestMapping(method = RequestMethod.PUT, value = "/downloadFile/{fileName:.+}")
+    @GetMapping(value = "/downloadFile/{fileId}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable Long fileId, HttpServletRequest request) {
+        // Load file as Resource
+        DBFile dbFile = fileService.getFile(fileId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(dbFile.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dbFile.getFileName() + "\"")
+                .body(new ByteArrayResource(dbFile.getData()));
+    }
+}
